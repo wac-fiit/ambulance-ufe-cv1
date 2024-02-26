7cd5c3353ca1e8de3b59db038cf541a431ed613b
Peter Micuch <peter.micuch@siemens-healthineers.com>
2024-02-26 11:22:44 +0000

    entry editor and navigation

--- src/components/pmi-ambulance-wl-list/pmi-ambulance-wl-list.tsx
+++ src/components/pmi-ambulance-wl-list/pmi-ambulance-wl-list.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Event, EventEmitter,  Host, h } from '@stencil/core';
 
 @Component({
   tag: 'pmi-ambulance-wl-list',
@@ -6,7 +6,7 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class PmiAmbulanceWlList {
-
+  @Event({ eventName: "entry-clicked"}) entryClicked: EventEmitter<string>;
   waitingPatients: any[];
 
   private async getWaitingPatientsAsync(){
@@ -44,8 +44,8 @@ export class PmiAmbulanceWlList {
     return (
       <Host>
         <md-list>
-          {this.waitingPatients.map(patient =>
-            <md-list-item>
+        {this.waitingPatients.map((patient, index) =>
+            <md-list-item onClick={ () => this.entryClicked.emit(index.toString())}>
               <div slot="headline">{patient.name}</div>
               <div slot="supporting-text">{"Predpokladaný vstup: " + this.isoDateToLocale(patient.estimatedStart)}</div>
                 <md-icon slot="start">person</md-icon>
